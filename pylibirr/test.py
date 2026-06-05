c85069b8a04e526ddb0e86829b429c9660a28b4c
tuna2134 <tuna2134@neody.land>
2026-06-06 08:01:57 +0900

    Add additional fetch_routes call in main function of test.py

--- pylibirr/test.py
+++ pylibirr/test.py
@@ -7,5 +7,8 @@ async def main() -> None:
     routes = await pylibirr.fetch_routes(152873)
     print(routes)
 
+    routes = await pylibirr.fetch_routes(63800)
+    print(routes)
+
 
 asyncio.run(main())
